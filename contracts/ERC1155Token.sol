77bc283976cf5acd96f58c41a0b6f50dc0237935
Bogdan Luckyanchuk <ibogdan533@gmail.com>
2024-10-06 23:16:43 +0300

    update CustomERC1155 baseUri

--- contracts/ERC1155Token.sol
+++ contracts/ERC1155Token.sol
@@ -14,7 +14,7 @@ contract CustomERC1155 is ERC1155, Ownable {
         address initialOwner
     )
         ERC1155(
-            "https://opensea.io/assets/ethereum/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d/5265"
+            "https://opensea.io/assets/ethereum/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d/"
         )
         Ownable(initialOwner)
     {}
